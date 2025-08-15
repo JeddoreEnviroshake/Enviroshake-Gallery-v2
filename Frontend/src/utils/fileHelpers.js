176a76f0ad6e9fe61ed251777f3da398990c5ab9
Bharath-S10 <sigdelbharath1@gmail.com>
2025-08-14 23:56:13 -0400

    feat: add backend upload helpers

--- Frontend/src/utils/fileHelpers.js
+++ Frontend/src/utils/fileHelpers.js
@@ -1,14 +1,14 @@
+import { imageUrlFromKey } from "../services/api";
+
 export const getFileExt = (fileName) => {
   if (!fileName) return "";
   const idx = fileName.lastIndexOf(".");
   return idx !== -1 ? fileName.substring(idx) : "";
 };
 
-const BUCKET_URL = "https://enviroshake-gallery-images.s3.amazonaws.com";
-
 export const srcFromImage = (img) => {
   if (!img) return "";
   if (img.s3Url) return img.s3Url;
-  if (img.s3Key) return `${BUCKET_URL}/${img.s3Key}`;
+  if (img.s3Key) return imageUrlFromKey(img.s3Key);
   return img.url || "";
 };
--- Frontend/src/utils/imageUrl.js
+++ Frontend/src/utils/imageUrl.js
@@ -1,8 +1,8 @@
-const BUCKET_URL = "https://enviroshake-gallery-images.s3.amazonaws.com";
+import { imageUrlFromKey } from "../services/api";
 
 export const srcFromImage = (img) => {
   if (!img) return "";
   if (img.s3Url) return img.s3Url;
-  if (img.s3Key) return `${BUCKET_URL}/${img.s3Key}`;
+  if (img.s3Key) return imageUrlFromKey(img.s3Key);
   return img.url || "";
 };
